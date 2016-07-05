6cb191edd112835fb449a643d44ccc2f53aadfe6
lpessagno <leslibethpessagno@gmail.com>
2016-07-04 20:24:38 -0500

    Pantalla de Rest. v1

---
+++ app/src/main/java/com/restaurante/cibertec/apprestaurante/ResturantActivity.java
@@ -0,0 +1,46 @@
+package com.restaurante.cibertec.apprestaurante;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class ResturantActivity extends AppCompatActivity {
+
+    TabHost tabHost;
+    TabHost.TabSpec tabSpec;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resturant);
+
+        tabHost=(TabHost)findViewById(R.id.tabpanel);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1 = tabHost.newTabSpec("Comentarios");
+
+        spec1.setContent(R.id.tab_comentarios);
+        spec1.setIndicator("Reseñas");
+
+        tabHost.addTab(spec1);
+
+
+        TabHost.TabSpec spec2 = tabHost.newTabSpec("Fotos");
+
+        spec2.setContent(R.id.panel_fotos);
+        spec2.setIndicator("Fotos");
+
+        tabHost.addTab(spec2);
+
+        TabHost.TabSpec spec3 = tabHost.newTabSpec("Menú");
+
+        spec3.setContent(R.id.tab_menu);
+        spec3.setIndicator("Menu");
+
+        tabHost.addTab(spec3);
+
+
+
+    }
+}
