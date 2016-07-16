f91dc5b6f826970a88fb80f230fc7df19084ea52
jkrentzien <jkdsz9@gmail.com>
2016-07-16 13:03:43 -0400

    clases pojo user y restaurant

---
+++ app/src/main/java/Restaurant.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+/**
+ * Created by Jorge on 7/9/2016.
+ */
+public class Restaurant implements Serializable {
+
+    public Restaurant(){}
+
+
+}
---
+++ app/src/main/java/User.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+/**
+ * Created by Jorge on 7/9/2016.
+ */
+public class User implements Serializable {
+
+    public User(){}
+}
